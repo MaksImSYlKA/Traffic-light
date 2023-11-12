1679e0dc48d671081bbe349d25c147ae3a593451
Maxim Sylka <msylka@me.com>
2023-11-12 18:15:41 +0300

    Create views and connect outlets

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  
     }
 
-
+    
+    @IBAction func switchTraffiLight(_ sender: Any) {
+    }
+    
 }
 
